e7fecb8a0a027215e74470ac57f29cb8676490dd
emmygozi <mygitmail@yahoo.com>
2018-07-19 00:16:11 +0100

    [Feature #159156539] Add error handlers for express routes with log files

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import routes from './routes/index';
+import error from './middlewares/routeErrors';
 
 
 const app = express();
@@ -13,6 +15,10 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(helmet());
+
+
+app.use('/', routes);
+app.use(error);
 
 
 const port = process.env.PORT || 8000;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
+import diary from '../dummyControllers/myDiaryController';
 
 
 const router = express.Router();
 
-router.get('/api/v1/diary');
-router.get('/api/v1/diary/:id');
-router.post('/api/v1/diary');
-router.put('/api/v1/diary/:id');
-router.delete('/api/v1/diary/:id');
+router.get('/api/v1/diary', diary.getEntry);
+router.get('/api/v1/diary/:id', diary.getOneEntry);
+router.post('/api/v1/diary', diary.postEntry);
+router.put('/api/v1/diary/:id', diary.updateEntry);
+router.delete('/api/v1/diary/:id', diary.removeAnEntry);
 
-// router.use('/api/v1/diary', courses);
 
 export default router;
