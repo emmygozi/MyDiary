dbb351ba9645d3bfc37c69b2e52a86c88e0136e7
emmygozi <mygitmail@yahoo.com>
2018-07-18 23:21:06 +0100

    [Feature #159150787] Get specific user entry

--- server/dummyControllers/myDiaryController.js
+++ server/dummyControllers/myDiaryController.js
@@ -6,6 +6,18 @@ class Entry {
   static getEntry(_req, res) {
     res.send(entry);
   }
+
+  static getOneEntry(req, res) {
+    // eslint-disable-next-line
+    if (req.params.id != parseInt(req.params.id, 10)) return res.status(400)
+      .send('Given ID is not a number!');
+
+
+    const myEntryIndex = entry.findIndex(c => c.id === parseInt(req.params.id, 10));
+    if (myEntryIndex === -1) return res.status(404).send('The entry with th given ID was not found!');
+
+    res.send(entry[myEntryIndex]);
+  }
 }
 
 export default Entry;
