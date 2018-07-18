6de5b7684fbf82a3c67aeb9ac2d9464d59a4a2f0
emmygozi <mygitmail@yahoo.com>
2018-07-18 23:40:06 +0100

    [Feature #159150899] make a new diary entry

--- server/dummyControllers/myDiaryController.js
+++ server/dummyControllers/myDiaryController.js
@@ -1,11 +1,35 @@
 import entry from '../dummyModels/entry';
-
+import validateEntry from '../middlewares/validateEntry';
 
 class Entry {
   // we put underscore '_' in front of req to show that it is intentionally unused
   static getEntry(_req, res) {
     res.send(entry);
   }
+
+  static postEntry(req, res) {
+    const { error } = validateEntry(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const {
+      title, message, userid,
+    } = req.body;
+    const id = entry[entry.length - 1].id + 1;
+
+    const anEntry = {
+      id,
+      title,
+      message,
+      userid
+    };
+
+    const foundEntry = entry.find(myentry =>
+      (myentry.title.toLowerCase() === title.toLowerCase()));
+
+    if (foundEntry) return res.status(409).send(`An entry with title '${title}' is already in the entry options`);
+    entry.push(anEntry);
+    res.send(entry);
+  }
+
 
   static getOneEntry(req, res) {
     // eslint-disable-next-line
